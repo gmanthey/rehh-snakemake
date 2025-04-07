67b7f360363f4c920df1974adb7e619b658b0a07
Georg Langebrake <g.langebrake@posteo.de>
2025-04-07 16:38:24 +0200

    initial commit

---
+++ workflow/scripts/scan_hh.R
@@ -0,0 +1,15 @@
+library(rehh)
+
+hh <- data2haplohh(hap_file = snakemake@input[[1]],
+                   polarize_vcf = FALSE,
+                   min_maf = snakemake@config[["min_maf"]],
+                   chr.name = snakemake@wildcards[["chromosome"]],
+                   vcf_reader = "vcfR")
+
+scan <- scan_hh(hh, polarized=FALSE)
+
+save(scan, file=snakemake@output[[1]])
+
+ihs <- ihh2ihs(scan)
+
+write.csv(ihs, snakemake@output[[2]])
---
+++ workflow/scripts/xpehh.R
@@ -0,0 +1,11 @@
+library(rehh)
+
+ihh_pop1 <- load(snakemake@input[[1]])
+ihh_pop2 <- load(snakemake@input[[2]])
+
+xpehh <- ies2xpehh(scan_pop1 = ihh_pop1, 
+                   scan_pop2 = ihh_pop2,
+                   popname1 = snakemake@wildcards[["pop1"]],
+                   popname2 = snakemake@wildcards[["pop2"]])
+
+write.csv(xpehh, snakemake@output[[1]])
